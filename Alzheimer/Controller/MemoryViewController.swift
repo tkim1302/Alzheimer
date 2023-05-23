9955152cbb802977b7d456bb2c736b4cd051c699
kimh1235 <114215667+hgkim0313@users.noreply.github.com>
2023-05-23 22:10:15 +1000

    jkjkja

--- Alzheimer/Controller/MemoryViewController.swift
+++ Alzheimer/Controller/MemoryViewController.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+//
+//  ViewController.swift
+//  Alzheimer
+//
+//  Created by 김태영 on 2023/05/22.
+//
+
+import UIKit
+
+class MemoryViewController: UIViewController {
+
+    
+    @IBOutlet weak var informationLabel: UILabel!
+    
+    var name = ""
+    var homeAddress = ""
+    var emergencyContact = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        if(name == ""){
+            informationLabel.text = "Not yet"
+        }else{
+            informationLabel.text = name
+        }
+        
+    }
+
+    @IBAction func goToMemoryButton(_ sender: Any) {
+    }
+    
+    @IBAction func goToRepeatButton(_ sender: Any) {
+    }
+    
+    @IBAction func goToSettingsButton(_ sender: Any) {
+    }
+    
+}
+
