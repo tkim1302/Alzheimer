2978a410f8cc337ddd3f4374ff4974d1cf727bf3
tkim1302 <tkim1302@users.noreply.github.com>
2023-05-23 18:29:17 +1000

    test commit

--- Alzheimer/Controller/ViewController.swift
+++ Alzheimer/Controller/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var homeAdrressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if(homeAdrressLabel.text == nil){
+            homeAdrressLabel.text = "No home address is set yet"
+        }
+        homeAdrressLabel.text = "Home"
     }
 
 
