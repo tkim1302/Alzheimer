601a50ed9fc328f833d928aaeabe762e622e1a95
tkim1302 <tkim1302@users.noreply.github.com>
2023-05-29 20:41:47 +1000

    20:41 5/29

--- Alzheimer/Controller/RepeatingTaskViewController.swift
+++ Alzheimer/Controller/RepeatingTaskViewController.swift
@@ -12,11 +12,6 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let userDefaults = UserDefaults.standard
-//        if let storedModels = userDefaults.stringArray(forKey: "Models") {
-//            self.models = storedModels
-//        }
-//
         repeatingTask.delegate = self
         repeatingTask.dataSource = self
         
@@ -32,10 +27,19 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-            reloadData()
-            
-            let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonPressed))
-            navigationItem.leftBarButtonItem = backButton
+        reloadData()
+        
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonPressed))
+        navigationItem.leftBarButtonItem = backButton
+        
+        for row in 0..<models.count {
+            let switchKey = "SwitchValue_\(row)"
+            let switchValue = UserDefaults.standard.object(forKey: switchKey) as? Bool
+            if let switchControl = repeatingTask.cellForRow(at: IndexPath(row: row, section: 0))?.accessoryView as? UISwitch {
+                switchControl.isOn = switchValue ?? true
+                switchValueChanged(switchControl)
+            }
+        }
     }
     
     func reloadData() {
@@ -55,42 +59,37 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = repeatingTask.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = tasks[indexPath.row] // Clear the previous text value
-        cell.detailTextLabel?.text = models[indexPath.row] // Set the subtitle text
+        cell.textLabel?.text = tasks[indexPath.row]
+        cell.detailTextLabel?.text = models[indexPath.row]
         
         cell.textLabel?.font = UIFont(name: "Arial", size: 22)
-        cell.detailTextLabel?.font = UIFont(name: "Arial", size: 16) // Set the font for the subtitle
+        cell.detailTextLabel?.font = UIFont(name: "Arial", size: 16)
         
-        if repeatingTask.isEditing {
-            // Hide the switch when editing
+        if repeatingTask.isEditing { //hide the switch when editing
             cell.accessoryView = nil
         } else {
-            // Show the switch when not editing
-            
-            // Check if the switch value exists in UserDefaults
             let switchKey = "SwitchValue_\(indexPath.row)"
             let switchValue = UserDefaults.standard.object(forKey: switchKey) as? Bool
             
-            // If switch value is nil, set the default value to true
+
             let defaultValue = switchValue ?? true
-            
-            // Create a UISwitch
             let switchControl = UISwitch()
             
-            // Set the switch value
             switchControl.isOn = defaultValue
             
-            // Add a target-action to handle switch value changes
+
             switchControl.addTarget(self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
             
-            // Set the frame for the switch
+            
             switchControl.frame = CGRect(x: cell.contentView.frame.size.width - switchControl.frame.size.width - 16, y: (cell.contentView.frame.size.height - switchControl.frame.size.height) / 2, width: switchControl.frame.size.width, height: switchControl.frame.size.height)
             
-            // Assign a tag to the switch based on the row index
+           
             switchControl.tag = indexPath.row
             
-            // Set the switch as the accessory view of the cell
+           
             cell.accessoryView = switchControl
+            
+            switchValueChanged(switchControl)
         }
         
         return cell
@@ -99,7 +98,7 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
     @objc func switchValueChanged(_ sender: UISwitch) {
         let rowIndex = sender.tag
         
-        // Update your data model based on the switch value
+      
         let switchValue = sender.isOn
         UserDefaults.standard.set(switchValue, forKey: "SwitchValue_\(rowIndex)") // Store the switch value in UserDefaults
         
@@ -110,14 +109,52 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "HH:mm"
             
-            if let date = dateFormatter.date(from: timeModel) {
-                addNotification(at: date, forTask: taskModel)
+            guard let targetTime = dateFormatter.date(from: timeModel) else {
+                print("Invalid time format")
+                return
+            }
+            
+            let now = Date()
+            let calendar = Calendar.current
+            
+            let targetComponents = calendar.dateComponents([.hour, .minute], from: targetTime)
+            let currentComponents = calendar.dateComponents([.hour, .minute], from: now)
+            
+            var triggerComponents = DateComponents()
+            triggerComponents.hour = targetComponents.hour
+            triggerComponents.minute = targetComponents.minute
+            
+            if let currentHour = currentComponents.hour, let targetHour = targetComponents.hour {
+                if currentHour > targetHour || (currentHour == targetHour && currentComponents.minute! > targetComponents.minute!) {
+                    // If the target time has already passed today, schedule it for tomorrow
+                    triggerComponents.day = calendar.component(.day, from: now) + 1
+                } else {
+                    // Schedule it for today
+                    triggerComponents.day = calendar.component(.day, from: now)
+                }
+                
+                let trigger = UNCalendarNotificationTrigger(dateMatching: triggerComponents, repeats: false)
+                
+                let content = UNMutableNotificationContent()
+                content.title = "Alarm"
+                content.body = taskModel
+                content.sound = UNNotificationSound.default
+                
+                let request = UNNotificationRequest(identifier: taskModel, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request) { error in
+                    if let error = error {
+                        print("Error adding notification: \(error.localizedDescription)")
+                    } else {
+                        print("Notification added successfully")
+                    }
+                }
             }
         } else {
             let taskModel = tasks[rowIndex]
             cancelNotification(forTask: taskModel)
         }
     }
+
 
 
     
@@ -132,7 +169,7 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
             tableView.deleteRows(at: [indexPath], with: .fade)
             
             // Update the models array in UserDefaults
-            
+            reloadData()
         }
     }
     
@@ -164,14 +201,13 @@ class RepeatingTaskViewController: UIViewController, UITableViewDelegate, UITabl
             repeatingTask.isEditing = true
         }
     }
-    func addNotification(at date: Date, forTask task: String) {
+    func addNotification(at time: TimeInterval, forTask task: String) {
         let content = UNMutableNotificationContent()
         content.title = "Alarm"
         content.body = task
         content.sound = UNNotificationSound.default
         
-        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
-        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
         
         let request = UNNotificationRequest(identifier: task, content: content, trigger: trigger)
         
