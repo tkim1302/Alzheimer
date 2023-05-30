701e025add7c45a379dc1fd8bc6717508becb163
kimh1235 <114215667+hgkim0313@users.noreply.github.com>
2023-05-30 20:45:26 +1000

    Revert "Untitled Pull Request"

--- Alzheimer/Controller/MemoryViewController.swift
+++ Alzheimer/Controller/MemoryViewController.swift
@@ -3,32 +3,45 @@ import CoreData
 import Photos
 import AVFoundation
 
-
 class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, NSFetchedResultsControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AlbumCellDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBOutlet weak var mainName: UILabel!
+    
+    // Audio recording properties
     var audioRecorder: AVAudioRecorder!
     var audioPlayer: AVAudioPlayer!
+    
+    // Properties for tracking the current cell and album name
     var currentCell: AlbumCollectionViewCell?
     var currentAlbumName: String?
     
+    // NSFetchedResultsController for managing the fetched albums
     var fetchedResultsController: NSFetchedResultsController<Albums>!
+    
+    // Flag for indicating the edit mode
     var isEditMode = false
+    
+    // Managed context for Core Data operations
     let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // Image picker controller for selecting photos
     let imagePickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mainName.text =  "Love Ones <3"
+        
+        // Register the custom cell nib for the collection view
         let nib = UINib(nibName: "AlbumCollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "AlbumCell")
         
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.allowsMultipleSelection = true
-
+        
+        // Fetch the albums from Core Data using NSFetchedResultsController
         let fetchRequest: NSFetchRequest<Albums> = Albums.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
         
@@ -41,11 +54,14 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
             print("Could not fetch. \(error), \(error.userInfo)")
         }
         
+        // Configure the image picker controller
         imagePickerController.delegate = self
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
         imagePickerController.allowsEditing = true
     }
+    
+    //  - UICollectionViewDataSource
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return fetchedResultsController.sections?.count ?? 0
@@ -60,24 +76,26 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCollectionViewCell
         cell.delegate = self
-
+        
+        // Get the album for the current index path
         let album = fetchedResultsController.object(at: indexPath)
-
+        
+        // Configure the cell with album data
         cell.nameLabel.text = album.name
         cell.audioFilename = album.audioFilename
+        
         if let imageData = album.coverImage, let image = UIImage(data: imageData) {
             cell.imageView.image = image
         } else {
             cell.imageView.image = nil // or a placeholder image
         }
-
+        
         cell.contentView.backgroundColor = .white // Resetting cell's color to default.
-
+        
         return cell
     }
-
-
-
+    
+    // - UICollectionViewDelegate
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if isEditMode {
@@ -98,22 +116,28 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
         }
     }
     
+    //  Action Methods
+    
     @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
         isEditMode = !isEditMode
         sender.title = isEditMode ? "Done" : "Edit"
         collectionView.allowsMultipleSelection = isEditMode
         
         if isEditMode {
+            // Show options for add person, delete person, or cancel
             let alertController = UIAlertController(title: "Options", message: "Please select an option", preferredStyle: .actionSheet)
             
+            // Add Person action
             let addPersonAction = UIAlertAction(title: "Add Person", style: .default) { _ in
-                let albumController = UIAlertController(title: "New Album", message: "Enter a name of your love one", preferredStyle: .alert)
+                let albumController = UIAlertController(title: "New Album", message: "Enter a name of your loved one", preferredStyle: .alert)
                 albumController.addTextField { textField in
                     textField.placeholder = "Album name"
                 }
                 let addAction = UIAlertAction(title: "Add", style: .default) { _ in
                     let albumName = albumController.textFields?.first?.text ?? ""
                     self.currentAlbumName = albumName
+                    
+                    // Create a new album entity and save it to Core Data
                     let newAlbum = Albums(context: self.managedContext)
                     newAlbum.name = albumName
                     
@@ -122,35 +146,36 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
                     } catch let error as NSError {
                         print("Could not save. \(error), \(error.userInfo)")
                     }
-
+                    
                     // After adding the album, present the image picker
                     self.present(self.imagePickerController, animated: true, completion: nil)
                 }
-
+                
                 albumController.addAction(addAction)
                 let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
                 albumController.addAction(cancelAction)
                 self.present(albumController, animated: true)
             }
             
+            // Delete Person action
             let deleteAction = UIAlertAction(title: "Delete Person", style: .destructive)
             
-            let addPictureAction = UIAlertAction(title: "Add Pictures", style: .default)
-            
+            // Cancel action
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
             
             alertController.addAction(addPersonAction)
             alertController.addAction(deleteAction)
-            alertController.addAction(addPictureAction)
             alertController.addAction(cancelAction)
             
             present(alertController, animated: true)
         } else {
             if let selectedIndexPaths = self.collectionView.indexPathsForSelectedItems {
                 for indexPath in selectedIndexPaths {
+                    // Delete the selected albums from Core Data
                     let albumToDelete = self.fetchedResultsController.object(at: indexPath)
                     self.managedContext.delete(albumToDelete)
                 }
+                
                 do {
                     try self.managedContext.save()
                     collectionView.reloadData()
@@ -160,27 +185,34 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
             }
         }
     }
-
+    
+    // - NSFetchedResultsControllerDelegate
     
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         collectionView.reloadData()
     }
     
+    //  - Navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowPhotosSegue",
            let destinationVC = segue.destination as? PhotoViewController,
            let indexPath = sender as? IndexPath {
+            // Pass the selected album data to the PhotoViewController
             let selectedAlbum = fetchedResultsController.object(at: indexPath)
             destinationVC.selectedAlbum = selectedAlbum
             destinationVC.albumName = selectedAlbum.name
         }
     }
+    
+    // - Audio Recording Methods
+    
     func startRecording(albumName: String) {
         let audioFilename = "\(albumName).m4a"
-
+        
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let audioURL = documentsDirectory.appendingPathComponent(audioFilename)
-
+        
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
@@ -188,14 +220,14 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
         self.currentAlbumName = audioFilename
-
+        
         do {
             audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
             audioRecorder.record()
-
+            
             // Save audioFilename for later use
             currentCell?.audioFilename = audioFilename
-
+            
             // Provide visual feedback that recording has started
             let alertController = UIAlertController(title: "Recording...", message: "Press Stop when you're done recording.", preferredStyle: .alert)
             let stopAction = UIAlertAction(title: "Stop", style: .default) { _ in
@@ -203,29 +235,29 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
             }
             alertController.addAction(stopAction)
             self.present(alertController, animated: true, completion: nil)
-
+            
             print("Started recording audio at URL: \(audioURL)")
         } catch {
             finishRecording(success: false)
         }
     }
-
+    
     func finishRecording(success: Bool) {
         if let recorder = audioRecorder {
             recorder.stop()
             audioRecorder = nil
         }
-
+        
         if success {
             print("Recording successful.")
-
+            
             guard let albumName = self.currentAlbumName else { return }
             let audioURL = getDocumentsDirectory().appendingPathComponent(albumName)
-
+            
             // Check if the audio file exists at the URL
             let fileExists = FileManager.default.fileExists(atPath: audioURL.path)
             print("Audio file exists at URL: \(fileExists), URL: \(audioURL)")
-
+            
             let fetchRequest: NSFetchRequest<Albums> = Albums.fetchRequest()
             fetchRequest.predicate = NSPredicate(format: "name == %@", albumName)
             if let result = try? self.managedContext.fetch(fetchRequest), let album = result.first {
@@ -242,45 +274,45 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
             print("Recording failed.")
         }
     }
-
-
-
-
-
-
+    
+    //  - Helper Methods
+    
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
-
- 
-
+    
+    //  - UIImagePickerControllerDelegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true, completion: nil)
-
+        
         guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
             print("Error: \(info)")
             return
         }
-
+        
         guard let imageData = selectedImage.jpegData(compressionQuality: 1) else {
             print("Could not convert image to Data.")
             return
         }
-
+        
         let fetchRequest: NSFetchRequest<Albums> = Albums.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "name == %@", self.currentAlbumName ?? "")
+        
         do {
             let fetchedResults = try self.managedContext.fetch(fetchRequest)
+            
             if let currentAlbum = fetchedResults.first {
                 currentAlbum.coverImage = imageData
+                
                 if let audioFilename = self.currentAlbumName {
                     print("Saving audio filename: \(audioFilename)")
                     currentAlbum.audioFilename = audioFilename
                 } else {
                     print("No audio filename to save")
                 }
+                
                 try self.managedContext.save()
             } else {
                 print("Could not fetch the current album.")
@@ -288,48 +320,48 @@ class MemoryViewController: UIViewController, UICollectionViewDelegate, UICollec
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
-
+        
         if let albumName = currentAlbumName {
             startRecording(albumName: albumName)
         }
     }
-
-
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
-
     }
     
-    
-
-
 }
 
+// - UICollectionViewDelegateFlowLayout
+
 extension MemoryViewController: UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 150, height: 220)
     }
 }
 
-extension MemoryViewController{
+//  - AlbumCellDelegate
 
+extension MemoryViewController {
+    
     func playButtonTapped(cell: AlbumCollectionViewCell) {
         // Set the current cell
         currentCell = cell
-
+        
         // Play the recording for this album
         if let audioFilename = cell.audioFilename {
             let audioURL = getDocumentsDirectory().appendingPathComponent(audioFilename)
-
+            
             let audioFileURL = audioURL.appendingPathExtension("m4a")
             print("Audio URL: \(audioFileURL)")
-
+            
             // Verify the audio file exists at the path
             if !FileManager.default.fileExists(atPath: audioFileURL.path) {
                 print("No file found at path: \(audioFileURL.path)")
                 return
             }
-
+            
             // Check the size of the audio file
             do {
                 let resources = try audioFileURL.resourceValues(forKeys: [.fileSizeKey])
@@ -342,7 +374,7 @@ extension MemoryViewController{
             } catch {
                 print("Error getting file size: \(error)")
             }
-
+            
             // Try to play the file
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL)
@@ -352,6 +384,4 @@ extension MemoryViewController{
             }
         }
     }
-
-
 }
--- Alzheimer/Controller/PhotoViewController.swift
+++ Alzheimer/Controller/PhotoViewController.swift
@@ -3,84 +3,118 @@ import CoreData
 
 class PhotoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, NSFetchedResultsControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    // CollectionView and a label outlets
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mainNameLabel: UILabel!
     
+    // Core Data fetched results controller for Photos entity
     var fetchedResultsController: NSFetchedResultsController<Photos>!
+    // Boolean variable to toggle edit mode
     var isEditMode = false
+    // Album selected for showing photos in the collection view
     var selectedAlbum: Albums!
+    // Getting the managedContext for CoreData operations
     let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    // Variable to hold the name of the album
     var albumName: String?
     
+    // Overridden viewDidLoad method from UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Registering PhotoCollectionViewCell nib for collection view
         let nib = UINib(nibName: "PhotoCollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "PhotoCell")
         
+        // Setting delegate and data source for the collection view
         collectionView.delegate = self
         collectionView.dataSource = self
+        // Allowing multiple selection in the collection view
         collectionView.allowsMultipleSelection = true
 
+        // Creating fetch request for Photos entity
         let fetchRequest: NSFetchRequest<Photos> = Photos.fetchRequest()
+        // Fetching photos that belong to the selected album
         fetchRequest.predicate = NSPredicate(format: "albums == %@", selectedAlbum)
+        // Sorting photos by creation date
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
         
+        // Initialising fetchedResultsController
         fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
+        // Setting the fetchedResultsController delegate
         fetchedResultsController.delegate = self
         
+        // Attempting to fetch the photos
         do {
             try fetchedResultsController.performFetch()
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
         }
         
+        // Setting the main label to show album's name
         mainNameLabel.text = albumName
     }
     
+    // Collection view data source method for number of items in section
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // If there are fetched sections, return their count, else return 0
         guard let sections = fetchedResultsController.sections else { return 0 }
         let sectionInfo = sections[section]
         return sectionInfo.numberOfObjects
     }
     
+    // Collection view data source method for cell at index path
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Dequeue a reusable cell from the collection view
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCollectionViewCell
+        // Fetching the photo for the current index path
         let photo = fetchedResultsController.object(at: indexPath)
         
+        // If the photo has imageData, set it as the cell's image, else set a placeholder
         if let imageData = photo.imageData {
             cell.imageView.image = UIImage(data: imageData)
         } else {
             cell.imageView.image = nil // or some placeholder image
         }
+        // Setting the cell's background color
         cell.contentView.backgroundColor = .white
        
         return cell
     }
     
+    // Collection view delegate method when an item is selected
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // If in edit mode, change the cell's background color
         if isEditMode {
             let cell = collectionView.cellForItem(at: indexPath) as! PhotoCollectionViewCell
             cell.contentView.backgroundColor = .lightGray
         }
     }
     
+    // Collection view delegate method when an item is deselected
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        // If in edit mode, change the cell's background color back to white
         if isEditMode {
             let cell = collectionView.cellForItem(at: indexPath) as! PhotoCollectionViewCell
             cell.contentView.backgroundColor = .white
         }
     }
     
+    // Function to handle edit button tap
     @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        // Toggle edit mode
         isEditMode = !isEditMode
+        // Change sender title based on current edit mode status
         sender.title = isEditMode ? "Done" : "Edit"
+        // Set multiple selection based on edit mode status
         collectionView.allowsMultipleSelection = isEditMode
         
+        // If in edit mode, present an action sheet with options to add or delete photos
         if isEditMode {
             let alertController = UIAlertController(title: "Options", message: "Please select an option", preferredStyle: .actionSheet)
             
             let addPhotoAction = UIAlertAction(title: "Add Photo", style: .default) { _ in
+                // Present image picker when add photo is selected
                 let imagePickerController = UIImagePickerController()
                 imagePickerController.delegate = self
                 imagePickerController.sourceType = .photoLibrary
@@ -91,18 +125,22 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UICollect
             
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
             
+            // Add actions to the alert controller
             alertController.addAction(addPhotoAction)
             alertController.addAction(deleteAction)
             alertController.addAction(cancelAction)
             
+            // Present the alert controller
             present(alertController, animated: true)
         } else {
+            // If not in edit mode, delete all selected photos
             if let selectedIndexPaths = self.collectionView.indexPathsForSelectedItems {
                 for indexPath in selectedIndexPaths {
                     let photoToDelete = self.fetchedResultsController.object(at: indexPath)
                     self.managedContext.delete(photoToDelete)
                 }
                 do {
+                    // Save changes to managed context after deleting photos
                     try self.managedContext.save()
                 } catch {
                     print("An error occurred while trying to delete photos.")
@@ -111,32 +149,41 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UICollect
         }
     }
     
+    // Image picker delegate method for when an image is selected
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Getting selected image and converting to data
         guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
               let imageData = selectedImage.jpegData(compressionQuality: 1.0) else {
             return
         }
         
+        // Creating a new Photo entity with selected image data
         let newPhoto = Photos(context: self.managedContext)
         newPhoto.albums = self.selectedAlbum
         newPhoto.imageData = imageData
         newPhoto.creationDate = Date()
         
+        // Attempting to save new photo to managed context
         do {
             try self.managedContext.save()
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
         
+        // Dismiss image picker after an image is selected
         picker.dismiss(animated: true, completion: nil)
     }
     
+    // Fetched results controller delegate method for when the content changes
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        // Reload collection view data when content changes
         collectionView.reloadData()
     }
 }
 
+// Extension to handle layout for collection view
 extension PhotoViewController: UICollectionViewDelegateFlowLayout {
+    // Method to define size for items in the collection view
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 150, height: 200)
     }
